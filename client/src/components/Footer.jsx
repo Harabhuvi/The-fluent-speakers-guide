2652711b129e43741b1393d299ec6ff7f84bb27b
Bhuvi <129815639+Harabhuvi@users.noreply.github.com>
2025-07-09 15:27:17 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer-newname">@Copyright : RAUNAK ROY BARMAN , 2023</div>
+    <div className="footer-newname">@Copyright : Bhuvi , 2023</div>
   );
 };
 
